33f9da9e6043b42af6b86191291b09ffdc6c6a35
GitHub Action <action@github.com>
2022-09-10 05:22:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/leeyoonsam/androidmvvmplugintemplate/MyBundle.kt
+++ src/main/kotlin/com/github/leeyoonsam/androidmvvmplugintemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.leeyoonsam.androidmvvmplugintemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/leeyoonsam/androidmvvmplugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/leeyoonsam/androidmvvmplugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.leeyoonsam.androidmvvmplugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.leeyoonsam.androidmvvmplugintemplate.MyBundle
 
 class MyApplicationService {
 
